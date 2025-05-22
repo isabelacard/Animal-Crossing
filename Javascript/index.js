1cae488249c6aba3f2047c3a3f1f75fa97237320
Isabela Cardona <1111481156@u.icesi.edu.co>
2025-05-22 02:05:36 -0500

    Se agregaron arreglos en el quienes somos js - index

--- Javascript/index.js
+++ Javascript/index.js
@@ -16,26 +16,3 @@ const characters = [
         image: "./assets/characterisabela.png"
     }
 ];
-
-function renderCharacters() {
-    const container = document.getElementById("charactersContainer");
-
-    characters.forEach(character => {
-        const card = document.createElement("div");
-        card.classList.add("character-card");
-
-        card.innerHTML = `
-            <h1>${character.name}</h1>
-            <br>
-            <p>Gender: ${character.gender}</p>
-            <p>Species: ${character.species}</p>
-            <p>Quote: "${character.quote}"</p>
-            <p>Birthday: ${character.birthday}</p>
-            <img src="${character.image}" alt="${character.name}">
-        `;
-
-        container.appendChild(card);
-    });
-}
-
-document.addEventListener("DOMContentLoaded", renderCharacters);
