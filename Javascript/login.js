856cc71134ec43a5b31cef3d968b329dcb8b208b
Isabela Cardona <1111481156@u.icesi.edu.co>
2025-05-23 15:29:24 -0500

    se añadieron posibles usuarios al login para iniciar sesión - login

--- Javascript/login.js
+++ Javascript/login.js
@@ -1,32 +1,47 @@
+const usuarios = [
+    {
+        nombre: "Mariana",
+        apellido: "Cerón",
+        email: "mariana@gmail.com",
+        password: "1234"
+    },
+    {
+        nombre: "Isabela",
+        apellido: "Cardona",
+        email: "isabela@gmail.com",
+        password: "abcd"
+    },
+];
+localStorage.setItem('usuarios', JSON.stringify(usuarios));
+
+
 const loginForm = document.getElementById('loginForm');
 
 function ingresarUsuario(e) {
-    e.preventDefault()
-    const email = document.getElementById('user').value
-    const password = document.getElementById('password').value
-    const usuarios = JSON.parse(localStorage.getItem('usuarios')) || []
-    const existeUsuario = usuarios.find((usuario) => usuario.email === email)
+    e.preventDefault();
+    const email = document.getElementById('user').value;
+    const password = document.getElementById('password').value;
+    const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+    const existeUsuario = usuarios.find((usuario) => usuario.email === email);
 
     if (!existeUsuario) {
-        alert ("El usuario no existe, favor registrarlo!")
-        loginForm.reset()
-        return
-
+        alert("El usuario no existe, favor registrarlo!");
+        loginForm.reset();
+        return;
     }
 
     if (existeUsuario.password !== password) {
-        alert ("La contraseña es incorrecta!")
-        loginForm.reset()
-        return
-
+        alert("La contraseña es incorrecta!");
+        loginForm.reset();
+        return;
     }
+
     const usuarioLogueado = {
         nombreCompleto: existeUsuario.nombre + " " + existeUsuario.apellido,
         email: existeUsuario.email
-    }
-    localStorage.setItem('logueado',JSON.stringify(usuarioLogueado))
-    window.location.href = '/index.html'; 
-
+    };
+    localStorage.setItem('logueado', JSON.stringify(usuarioLogueado));
+    window.location.href = '/index.html';
 }
 
-loginForm.addEventListener('submit',ingresarUsuario)
+loginForm.addEventListener('submit', ingresarUsuario);
