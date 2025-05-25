911d16fd850745c7b292bd31bf6c5d959648872a
Isabela Cardona <1111481156@u.icesi.edu.co>
2025-05-25 04:17:12 -0500

    Se agrego el boton al formulario sign up - sign up

--- Javascript/signup.js
+++ Javascript/signup.js
@@ -35,3 +35,14 @@ const usuario = {
 }
 
 registro.addEventListener('submit', registrarUsuario);
+
+//SIGNUP ICON
+const togglePassword = document.getElementById("togglePassword");
+const password = document.getElementById("password");
+
+togglePassword.addEventListener("click", function () {
+    const type = password.getAttribute("type") === "password" ? "text" : "password";
+    password.setAttribute("type", type);
+    this.classList.toggle("fa-eye");
+    this.classList.toggle("fa-eye-slash");
+});
