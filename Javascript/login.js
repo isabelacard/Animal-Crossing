b3e07eac480b43d658ebd7739a7c50a048f1e8cc
Isabela Cardona <1111481156@u.icesi.edu.co>
2025-05-25 04:07:37 -0500

    se generaron cambios respecto al login y la contraseña - login

--- Javascript/login.js
+++ Javascript/login.js
@@ -50,3 +50,17 @@ const usuarioLogueado = {
 }
 
 loginForm.addEventListener('submit', ingresarUsuario);
+
+//LOGIN
+
+document.addEventListener("DOMContentLoaded", () => {
+    const togglePassword = document.getElementById("togglePassword");
+    const passwordInput = document.getElementById("password");
+
+    togglePassword.addEventListener("click", () => {
+        const type = passwordInput.getAttribute("type") === "password" ? "text" : "password";
+        passwordInput.setAttribute("type", type);
+        togglePassword.classList.toggle("fa-eye");
+        togglePassword.classList.toggle("fa-eye-slash");
+    });
+});
